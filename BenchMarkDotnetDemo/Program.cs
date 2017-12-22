1ed36bd1cf5eaec32b264e472f507fc720ec4fc2
Miguel Teheran <mteheran@contractors.accruent.com>
2017-12-22 16:54:33 -0500

    Short demo using random numerics

---
+++ BenchMarkDotnetDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Filters;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Running;
+using System;
+
+namespace BenchMarkDotnetDemo
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<RandomCustom>();
+
+            Console.ReadLine();
+        }
+    }
+
+    [CoreJob] // used to specify the system core (CoreJob), Mono (MonoJob) and dotnet classic (ClrJob)
+    [AnyCategoriesFilter("Run")] //execute all method  with mark NoRun
+    public class RandomCustom
+    {
+        [Params(50, 1000,50000,1000000)]
+        public int intlong { get; set; }
+
+        int getRandom()
+        {
+            int inRandom = 0;
+            for (int i = 0; i < intlong; i++)
+            {
+                inRandom = new Random().Next(i);
+            }
+
+            return inRandom;
+        }
+
+        [Benchmark]
+        [BenchmarkCategory("Run")]
+        public int GetintRandom() => getRandom();
+
+        [Benchmark]
+        [BenchmarkCategory("NoRun")]
+        public int GetintRandom2() => (getRandom() *2);
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# BenchMarkDemo
+Demo using BenchMarkDotnet as BenchMark tool in .Net
+
+
+BenchMark could be installed as NuGet 
+
+![alt](https://mteheran.files.wordpress.com/2017/12/benchmark1.png)
+
+
+The official Web site is [text](http://benchmarkdotnet.org/) 
+
+
+The most important features in BenchMarkDotNet
+
+Standard benchmarking routine: generating an isolated project per each benchmark method; auto-selection of iteration amount; warmup; overhead evaluation; statistics calculation; and so on.
+Supported runtimes: Full .NET Framework (4.6+), .NET Core (1.1+), Mono
+Supported languages: C#, F#, and Visual Basic
+Supported OS: Windows, Linux, MacOS
+Easy way to compare different environments (x86 vs x64, LegacyJit vs RyuJit, and so on; see: Jobs)
+Reports: markdown, csv, html, json, xml, plain text and png plots
+Advanced features: Baseline, Params
+Powerful diagnostics based on ETW events (see BenchmarkDotNet.Diagnostics.Windows)
